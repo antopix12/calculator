3804891ac6af950bafdcf215b562a8e61ebb0177
antopix <antopix33@gmail.com>
2025-01-31 12:26:57 -0800

    basic calculator display added, non-functional

--- script.js
+++ script.js
@@ -24,3 +24,33 @@ let operator = '';
 function operate (opr, x, y) {
   return opr(x, y);
 }
+
+// Create buttons
+
+const container = document.querySelector(".container");
+
+function createButton (content, color) {
+  const btn = document.createElement("div");
+  btn.classList.add("btn");
+  btn.textContent = `${content}`;
+  btn.style.backgroundColor = `${color}`;
+  container.appendChild(btn);
+}
+
+
+function createButtons () {
+  let otherButtons = ['+', '-', '*', '/', 'C', '='];
+
+  for (let i = 0; i < otherButtons.length - 2; i++) {
+    createButton(otherButtons[i], 'red');
+  }
+  for (let i = 9; i >= 0; i--) {
+    createButton(i, 'white');
+  }
+  for (let i = 4; i < otherButtons.length; i++) {
+    createButton(otherButtons[i], 'red');
+  }
+  
+}
+
+createButtons();
